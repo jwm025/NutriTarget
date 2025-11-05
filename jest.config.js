766c581117bf17a9859f609d4a97528a2f5595ae
jwm025 <jwm025@bravemail.uncp.edu>
2025-11-05 13:40:46 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
 export default {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  extensionsToTreatAsEsm: ['.js'],
+  preset: 'jest-preset-angular/preset' ? No, for vanilla:
+  Actually for vanilla ESM:
+  export default {
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.js'],
+    globals: {
+      'ts-jest': {
+        useESM: true
+      }
+    },
+  };
+Wait, no ts-jest. For vanilla JS ESM, 'type': 'module' is enough, but to be safe:
+export default {
+  testEnvironment: 'node',
+  transform: {},
 };
